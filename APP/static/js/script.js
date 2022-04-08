91f7a75b2d955b3a689a8c2a3415ade0a37942b4
NM <balazs200428@gmail.com>
2022-04-08 17:00:52 +0200

    works

--- APP/static/js/script.js
+++ APP/static/js/script.js
@@ -1,25 +1,19 @@
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener("DOMContentLoaded", main);
 
-async function main()
-{
-    let audio = new Audio('static/audio/rickroll.mp3');
-    setTimeout(rickrollsound(audio), 1000)
-
-    let test = document.getElementsByName("body");
-    test.addEventListener("mouseenter", rickrollsound(audio), false)
-
-    let rr = document.querySelector('.rickrolled');
+async function main() {
+    let rr = document.querySelector(".rickrolled");
     for (let i = 1; i < 4; i++) {
-        rr.src = 'static/images/' + i + ".png";
-        await new Promise(r => setTimeout(r, 500));
-        if (i==3)
-        {
-            i=0;
+        rr.src = "static/images/" + i + ".png";
+        await new Promise((r) => setTimeout(r, 500));
+        if (i == 3) {
+            i = 0;
         }
     }
-}
-
-function rickrollsound(audio)
-{
-    
+    /*
+    let audio = new Audio("static/audio/rickroll.mp3");
+    let test = document.getElementsByName("body");
+    test.onmouseover = function () {
+        audio.play();
+    };
+    */
 }
